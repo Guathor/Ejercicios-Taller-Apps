0f062e7730edb1fc87dc112fca9a76324328bc54
Wolfbar <herrera.cristopher@gmail.com>
2020-01-11 10:13:44 -0300

    first commit

--- Login/index.js
+++ Login/index.js
@@ -1,1 +1,29 @@
-console.log("hola mundo");
+const boton = window.document.querySelector('#boton');
+
+boton.addEventListener('click', mostrarDatos);
+
+
+function mostrarDatos () {
+    
+    const input1 = window.document.querySelector('#email');
+    const user = input1.value;
+    const input2 = window.document.querySelector('#contraseña');
+    const pass = input2.value;
+    
+    console.log(user);
+    input1.value = '';
+    input1.focus(); 
+
+    console.log(pass);
+    input2.value = '';
+    input2.focus(); 
+
+    if (user === 'ejemplo@gmail.com' & pass === '123456'){
+        console.log('Iniciando sesion...')   
+        window.location.assign('http://127.0.0.1:5500/WhatzApp/Index.html'); 
+    } else{
+        console.log('Email y/o contraseña incorrecta');
+    }
+    
+
+}
